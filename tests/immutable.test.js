80d3ec6be2dcdc4df1af906cbc403c7765a95509
Carburator 11 <p.vautherin@hotmail.com>
2018-05-17 09:13:41 +0200

    [Added] Immutable test vs spread

--- tests/immutable.test.js
+++ tests/immutable.test.js
@@ -2,6 +2,16 @@ import { Map } from 'immutable';
 
 test("Object should not be mutated", () => {
     const obj1 = Map({ a: 11, b: 12, c: 13 });
-    const copy = obj1.set('a', 15);
-    expect(obj1.get('a')).toBe(11)
+    const copy1 = obj1.set('a', 15);
+    expect(obj1.get('a')).toBe(11);
+    expect(copy1.get('a')).toBe(15);
 })
+
+test("...or we could spread object", () => {
+    const obj2 = { a: 11, b: 12, c: 13 };
+    const copy2 = {...obj2};
+    copy2.a = 15;
+    expect(obj2.a).toBe(11);
+    expect(copy2.a).toBe(15);
+})
+
